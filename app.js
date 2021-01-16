07aac5fa7a112b45e48b39f53d124249f959927e
pelosek2525 <64742622+pelosek2525@users.noreply.github.com>
2021-01-16 02:07:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+
+
+const zdroje = document.querySelector('.zdroje');
+const potrvrditBtn = document.querySelector("#potvrdit");
+const body = document.querySelector("body");
+let pocetZdroju = 0;
+let zdrojaky = Array();
+
+var agent = navigator.userAgent;
+
+if(!agent.includes("Mozilla"))
+{
+    console.log("Používáš mozilu");
+}
+else {
+    body.addEventListener('click', event => {
+        
+         if(event.target.className == "zdroj"){
+            
+            navigator.clipboard.readText()
+            .then(text => {
+                if(validateUrl(text)){
+                    event.target.value = text;
+                 }
+            })
+           
+            
+     
+         }
+});
+}
+function validateUrl(value) {
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+  };
+
+function NovyZdroj(){
+    pocetZdroju++;
+    let novy = document.createElement('input');
+    novy.type="url";
+    novy.setAttribute.name = "adf";
+    novy.className = `zdroj`;
+   novy.placeholder = `Zdroj ${pocetZdroju}`;
+   novy.name = `zdroj${pocetZdroju}`;
+   
+    if(pocetZdroju ==1)
+    novy.value = "http://www.prvky.com/14.html";
+   
+   zdroje.appendChild(novy);
+
+
+};
+potrvrditBtn.addEventListener('click',(e) => {
+
+    e.preventDefault();
+    var audio = new Audio('smoke.mp3');
+    audio.volume = 0.05;
+        
+    let kokot = $("ul").children("input");
+    let pole = kokot.toArray();
+    $.ajax({                                      
+        url: 'https://calm-badlands-55983.herokuapp.com/zdroje.php',       
+        type: "GET",
+        data: pole, 
+        beforeSend: function() {
+            $("#loading-image").show();
+            $("#stefan").hide();
+         },
+    }).done(function( msg ) {
+        //console.log(msg);
+        $("#loading-image").hide();
+        $("#stefan").attr("src","stefanwin.png");
+        $("#stefan").show(1000);
+        audio.play();
+        $("body,html").css("background-image", 'url(weed.jpg)');
+        $("#results").css("display", 'block');
+        $(".showresults").html(msg);
+
+        
+    });
+
+
+});
+
