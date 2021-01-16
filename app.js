f38976ac4ed8c4c32f6a2ed30e27077b9d15227f
pelosek2525 <64742622+pelosek2525@users.noreply.github.com>
2021-01-16 02:18:31 +0100

    updated app.js

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ let pocetZdroju = 0;
 let zdrojaky = Array();
 
 var agent = navigator.userAgent;
-
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
 if(!agent.includes("Mozilla"))
 {
     console.log("Používáš mozilu");
